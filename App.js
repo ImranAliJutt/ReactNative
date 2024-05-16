db2cf07d9e33aebbf8977da41f5ea0178d50d341
Imran Ali <143342290+ImranAliJutt@users.noreply.github.com>
2024-05-16 12:54:30 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import Register from './screens/Register';
+import Splash from './screens/Splash';
+import MainPage from './screens/MainPage';
+import Hamburherdetailpage from './screens/Hamburherdetailpage';
+import Cart from './screens/Cart';
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+         <Stack.Screen name="Register" component={Register} />
+         <Stack.Screen name="MainPage" component={MainPage} />
+         <Stack.Screen name="Hamburherdetailpage" component={Hamburherdetailpage} />
+         <Stack.Screen name="Cart" component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
